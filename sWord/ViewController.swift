87ae19e656e65f3a0c3a3cf03820ed57de8992b5
tarszewski <ava7ar.e@gmail.com>
2016-04-01 12:31:14 +0200

    Initial Commit

---
+++ sWord/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sWord
+//
+//  Created by Paweł Tarszewski on 01.04.2016.
+//  Copyright © 2016 Paweł Tarszewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
